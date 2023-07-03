fb58e3ad724cefeb53ef443ba1c2b8ffe4ab9449
Jordan J. Mugenyi <jordanjamesxvi@outlook.com>
2023-07-03 21:48:43 +0100

    Add files via upload

---
+++ Website/main.py
@@ -0,0 +1,12 @@
+from front import create_app
+# from auth import auth  # Import the auth blueprint
+
+app = create_app()
+
+# Register the auth blueprint
+# app.register_blueprint(auth)
+
+# ... Other configurations and routes ...
+
+if __name__ == '__main__':
+    app.run(debug=True)
